8eb56fcf945479aecbdce0a1044f0c8130818afd
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-01-09 17:16:59 +0300

    bank chain

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-class Coffee{
-  cost(){
-    return 5
+class Account {
+
+  setNext(account) {
+    this.successor = account
+  }
+
+  pay(amountToPay) {
+    if (this.canPay(amountToPay)) {
+      console.log(`Paid ${amountToPay} using ${this.name}`)
+    } else if (this.successor) {
+      console.log(`Cannot pay using ${this.name}. Proceeding...`)
+      this.successor.pay(amountToPay)
+    } else {
+      console.log('None of the accounts have enough balance')
+    }
+  }
+
+  canPay(amount) {
+    return this.balance >= amount
   }
 }
 
-
-const sugar = coffee => {
-  const cost = coffee.cost();
-  coffee.cost = () => cost + 1
+class Bank extends Account {
+  constructor(balance) {
+      super()
+      this.name = 'bank'
+      this.balance = balance
+  }
 }
 
-const small = coffee => {
-  const cost = coffee.cost()
-  coffee.cost = () => cost - 1
+class Paypal extends Account {
+  constructor(balance) {
+      super()
+      this.name = 'Paypal'
+      this.balance = balance
+  }
+}
+class Bitcoin extends Account {
+  constructor(balance) {
+      super()
+      this.name = 'bitcoin'
+      this.balance = balance
+  }
 }
 
-const large = coffee => {
-  const cost = coffee.cost()
-  coffee.cost = () => cost + 1
-}
+const bank = new Bank(100)          
+const paypal = new Paypal(200)     
+const bitcoin = new Bitcoin(300) 
 
-const withMilk = coffee => {
-  const cost = coffee.cost()
-  coffee.cost = () => cost + 2
-}
+bank.setNext(paypal)
+paypal.setNext(bitcoin)
 
-
-const largeWithMilk = coffee => {
-  large(coffee)
-  withMilk(coffee)
-  const cost = coffee.cost()
-  coffee.cost = () => cost
-}
-const coffee = new Coffee()
-largeWithMilk(coffee)
-console.log(coffee.cost())
+bank.pay(259)
