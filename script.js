82b564d03c4caa433ee6dbbf573eb293f40adbd2
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-01-09 17:10:19 +0300

    singleton counter class

---
+++ script.js
@@ -0,0 +1,28 @@
+class Counter  {
+  count = 0;
+  constructor(){
+    if(typeof Counter.instance === 'object'){
+      return Counter.instance
+    }
+    Counter.instance = this
+    return Counter.instance
+  }
+
+  getCounter(){
+    return this.count
+  }
+
+  incCounter(){
+    this.count++
+  }
+}
+
+let a = new Counter()
+let b = new Counter()
+
+
+a.incCounter()
+b.incCounter()
+
+console.log(a === b)
+console.log(b)
