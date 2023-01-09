87a7e694f450008724f2778b9e2e1f3e9832556e
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-01-09 17:14:19 +0300

    factory method with bmw

--- script.js
+++ script.js
@@ -1,77 +1,29 @@
-class IteratorClass {
-  constructor(data) {
-    this.index = -1;
-    this.data = data;
-    this.done = false;
-    this.checkDataType(data);
+class BMW {
+  constructor(model, propA, propB) {
+    this.model = model;
+    this.propA = propA;
+    this.propB = propB;
   }
+}
 
-  checkDataType(data) {
-    if (Array.isArray(data)) {
-      this.collectionLength = this.data.length - 1;
-      this.collection = this.data;
-      this.type = 'array';
-    } else if (!Array.isArray(data) && typeof data == 'object') {
-      this.collectionLength = Object.keys(this.data).length - 1;
-      this.collection = Object.values(this.data);
-      this.type = 'object';
-    } else {
-      throw new Error(`${this.constructor.name} получил data которую не может итерировать - ${typeof data}`)
-    }
-  }
-
-  next() {
-    if (this.index < this.collectionLength) {
-      this.index++;
-      return {
-        key: Object.keys(this.data)[this.index],
-        value: this.collection[this.index],
-        done: false,
-        index: this.index,
-        type: this.type
-      }
-    } else {
-      this.done = true;
-      return {
-        done: true,
-        msg: 'достигнут конец коллекции',
-        index: this.index,
-        type: this.type
-      };
-    }
-  }
-
-  prev() {
-    if (this.done) {
-      this.done = false;
-      this.index = this.collectionLength + 1;
-    }
-    this.index--;
-    if (this.index > -1) {
-      return {
-        key: Object.keys(this.data)[this.index],
-        value: this.collection[this.index],
-        done: false,
-        index: this.index,
-        type: this.type
-      };
-    } else {
-      this.index = -1;
-      return {
-        done: this.done,
-        msg: 'достигнуто начало коллекции',
-        index: this.index,
-        type: this.type
-      };
+class BMWfactory {
+  create(model) {
+    switch (model) {
+      case 'X1':
+        return new BMW(model, 1, 2)
+        break;
+      case 'X2':
+        return new BMW(model, 3, 4)
+        break;
+      case 'X3':
+        return new BMW(model, 4, 5)
+        break;
     }
   }
 }
 
-const arr = new IteratorClass([1,2,3,4,5,6]);
-console.log(arr.next())
-console.log(arr.next())
-console.log(arr.next())
-console.log(arr.next())
-console.log(arr.next())
-console.log(arr.next())
-console.log(arr.next())
+const factory = new BMWfactory;
+const x1 = factory.create('X1');
+const x2 = factory.create('X2');
+console.log(x1)
+console.log(x2)
