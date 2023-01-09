dbf23b9b08f3a7a4543a091ab6af6bd4a7c04301
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-01-09 17:18:21 +0300

    abstract factory

--- script.js
+++ script.js
@@ -1,53 +1,29 @@
-class Account {
+class BMW_1_Series {}
 
-  setNext(account) {
-    this.successor = account
-  }
+class BMW_M_Series {
+  
+}
 
-  pay(amountToPay) {
-    if (this.canPay(amountToPay)) {
-      console.log(`Paid ${amountToPay} using ${this.name}`)
-    } else if (this.successor) {
-      console.log(`Cannot pay using ${this.name}. Proceeding...`)
-      this.successor.pay(amountToPay)
-    } else {
-      console.log('None of the accounts have enough balance')
-    }
-  }
 
-  canPay(amount) {
-    return this.balance >= amount
+
+function bmwFamilyfactory() {
+  return new BMW_1_Series()
+}
+function bmwSportFactory() {
+  return new BMW_M_Series()
+}
+
+// Abstract Factory
+function bmwProducer(type) {
+  switch (type) {
+    case 'sport': return bmwSportFactory;
+    case 'family': return bmwFamilyfactory;
+    default: return null;
   }
 }
 
-class Bank extends Account {
-  constructor(balance) {
-      super()
-      this.name = 'bank'
-      this.balance = balance
-  }
-}
+const sport = bmwProducer('sport');
+const family = bmwProducer('family');
 
-class Paypal extends Account {
-  constructor(balance) {
-      super()
-      this.name = 'Paypal'
-      this.balance = balance
-  }
-}
-class Bitcoin extends Account {
-  constructor(balance) {
-      super()
-      this.name = 'bitcoin'
-      this.balance = balance
-  }
-}
-
-const bank = new Bank(100)          
-const paypal = new Paypal(200)     
-const bitcoin = new Bitcoin(300) 
-
-bank.setNext(paypal)
-paypal.setNext(bitcoin)
-
-bank.pay(259)
+console.log(sport())
+console.log(family())
